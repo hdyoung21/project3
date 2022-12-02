4469780e1826194bb5224665c17eeca0415d4f59
hdyoung21 <112896050+hdyoung21@users.noreply.github.com>
2022-12-01 23:01:15 -0500

    mock data added

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -3,72 +3,12 @@ import User from '../models/user.js';
 import jwt from 'jsonwebtoken';
 import createError from '../utils/createError.js';
 
-<<<<<<< HEAD
 export const register = async (req, res) => {
 if(!req.body.name || !req.body.email || !req.body.password) {
     return res.json('Please fill in all of the inputs');
 }
 
 try{
-=======
-export const login = async (req, res, next) => {
-if(!req.body.email || !req.body.password) {
-    return next(
-        createError({
-            message: "Both fields are required.",
-            statusCode: 400,
-        }),
-    );  
-  }
-
-  try {
-    const user = await User.findOne({ email: req.body.email }).select(
-      'name email password',
-    );
-    if (!user) {
-      return next(
-        createError({ status: 404, message: 'User not found with the email' }),
-      );
-    }
-    const isPasswordCorrect = await bcryptjs.compare(
-      req.body.password,
-      user.password,
-    );
-    if (!isPasswordCorrect) {
-      return next(
-        createError({ status: 400, message: 'Password is incorrect' }),
-      );
-    }
-    const payload = {
-      id: user._id,
-      name: user.name,
-    };
-    const token = jwt.sign(payload, process.env.JWT_SECRET, {
-      expiresIn: '1d',
-    });
-    return res
-      .cookie('access_token', token, {
-        httpOnly: true,
-      })
-      .status(200)
-      .json({ name: user.name, email: user.email, message: 'login success' });
-  } catch (err) {
-    return next(err);
-  }
-};
-
-export const register = async (req, res, next) => {
-  if (!req.body.name || !req.body.email || !req.body.password) {
-    return next(
-      createError({
-        message: 'Name, Email & password are required',
-        statusCode: 400,
-      }),
-    );
-  }
-
-  try {
->>>>>>> window
     const salt = await bcryptjs.genSalt(10);
     const hashedPassword = await bcryptjs.hash(req.body.password, salt);
 
